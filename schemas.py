9eac9837a15f75587f03c4a540f717368d5abf2e
samin-Lee <sa030822@gmail.com>
2026-06-22 20:13:45 +0900

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import List
+
+class MakeupRecommendation(BaseModel):
+    lip: List[str]
+    blush: List[str]
+    eye: List[str]
+
+class AnalysisResponse(BaseModel):
+    status: str
+    season: str
+    tone: str
+    face_shape: dict
+    eye_metrics: dict
+    nose_metrics: dict
+    mouth_metrics: dict
+    makeup_recommendation: MakeupRecommendation
